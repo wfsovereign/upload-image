074e27d8db4c2cb3e931f01a02e0a7f0f869ec9c
wfsovereign <never_fall@sohu.com>
2015-07-28 23:13:15 +0800

    增加gm包，压缩图片测试通过

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
-
 /* GET home page. */
 
+var  gm =  require('gm').subClass({imageMagick: true});
+
+
+
 function router(app) {
-  app.get('/', function (req, res) {
-    res.render('index');
-  });
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+
+
+    app.get('/test', function (req, res) {
+        //res.render('index');
+        console.log('test');
+        gm('src/image/image.jpg')
+            .resize(100, 100)
+            .noProfile()
+            .write('public/image/resize.png', function (err) {
+                if (!err) console.log('done');
+                console.log('success');
+            });
+
+        res.end();
+    });
+
+
 
 }
 
